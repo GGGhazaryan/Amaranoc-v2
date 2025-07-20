a69879a79e83abd27deede0f1581d741ad9c4612
NeoGIXx <garik-gamer02@mail.ru>
2025-07-20 17:47:26 +0400

    optimizing leftContent/RightContent

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,12 @@ import './css/index.css';
 function App() {
   return (
     <div className="App">
-      <Header />
+    <Header />
       <div className="mainContentWrapper">
+          <div className="rightContentWrapper">
         <LeftContent />
         <RightContent />
-        
+        </div>
       </div>
        <GeneralFooter />
        <Footer/>
